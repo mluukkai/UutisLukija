3e767a0667cc71536882b0a174f79c840b943b53
Matti Luukkainen <mluukkai@iki.fi>
2015-09-05 22:46:17 +0300

    a test added

---
+++ src/test/java/LukijaTest.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author mluukkai
+ */
+public class LukijaTest {
+    
+    public LukijaTest() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @Test
+    public void testi(){
+        com.mycompany.uutislukija.Main.komennot();
+    }
+    
+}
