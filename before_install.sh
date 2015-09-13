170d3c01446e67046cf8bde75e03b8063fe5cb65
Pihla Toivanen <toivanenpihla@gmail.com>
2015-09-13 20:19:30 +0300

    travis konffit

---
+++ before_install.sh
@@ -0,0 +1,5 @@
+git clone https://github.com/UMTti/PaivanUutiset.git
+mvn clean install -U -f PaivanUutiset/pom.xml
+git clone https://github.com/UMTti/HackerNewsUutiset.git
+mvn clean install -U -f HackerNewsUutiset/pom.xml
+
